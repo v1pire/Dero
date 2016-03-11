fb0fbfa6c9f9e0f1cb0147e4331a1bcd276eb6b8
v1pire <bot.i.cow@gmail.com>
2016-03-12 00:07:16 +0300

    добавил уровень Elevator

---
+++ Assets/Scripts/CharacterCustomozator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCustomozator : MonoBehaviour {
+    public SpriteRenderer headSprite;
+    public Color32[] skinColors;
+    int skinColorCounter = 0;
+
+    public SpriteRenderer torseSprite;
+    public Color32[] torseColors;
+    int torseColorCounter = 0;
+
+    public SpriteRenderer legSprite;
+    public Color32[] legColors;
+    int legColorCounter = 0;
+
+    public void ChangeSkinColor()
+    { 
+        skinColorCounter++;
+        if(skinColorCounter>=skinColors.Length) skinColorCounter = 0;
+        headSprite.color = skinColors[skinColorCounter];
+    }
+
+    public void ChangeTorseColor()
+    {
+        torseColorCounter++;
+        if (torseColorCounter >= torseColors.Length) torseColorCounter = 0;
+        torseSprite.color = torseColors[torseColorCounter];
+    }
+
+    public void ChangeLegColor()
+    {
+        legColorCounter++;
+        if (legColorCounter >= legColors.Length) legColorCounter = 0;
+        legSprite.color = legColors[legColorCounter];
+    }
+}
---
+++ Assets/Scripts/Rotatior.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotatior : MonoBehaviour {
+    public float angle = 5;
+	
+	void FixedUpdate () {
+        transform.Rotate(Vector3.forward, angle * Time.deltaTime);
+	}
+}
