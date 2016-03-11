829e4d112da1cbb86ac57da3188f4d4af7402487
Stallker2003 <Stallker2003@gmail.com>
2016-03-12 00:01:15 +0200

    First Prototype Level Player Dies From Now

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public int PlayerHealth = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void HealthDamage(int Damage){
+		PlayerHealth -= Damage;
+		if (PlayerHealth <= 0) Application.LoadLevel (Application.loadedLevel);
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	int healthDamage;
+
+
+	// Use this for initialization
+	void Start () {
+		healthDamage = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	void OnCollisionStay2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			coll.gameObject.GetComponent<PlayerController> ().HealthDamage (healthDamage);
+	}
+		
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			coll.gameObject.GetComponent<PlayerController> ().HealthDamage (healthDamage);
+	} 
+}
